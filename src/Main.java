c7fb784da68bf406c6e9d426ee4b54c5e0699136
Kevin Franco <kevinarleyfg@gmail.com>
2025-08-29 18:02:24 -0500

    Codigos

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+   public static void main(string[]args) {
+       System.out.println("Hello world");
+       System.out.println("1:GIT STATUS- ESTADO RAMA");
+       System.out.println("2:GIT ADD-AGREGAR ARCHIVOS");
+       System.out.println("3:GIT COMMIT -M GUARDAR LOCAL");
+       System.out.println("4 GIT PUSH ORIGIN - SUBIR CAMBIOS");
+       System.out.println("5 GIT CLONE - CLONAR PROYECTO");
+       System.out.println("6 GIT BRANCH - VER LAS RAMAS");
+       System.out.println("7 GIT FETCH - RAMAS REMOTAS");
+       System.out.println("8 GIT SWITCH -C - CREAR Y POSICIONARME SOBRE LA RAMA");
+
+   }
+}
+
